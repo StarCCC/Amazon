c73f7b2a4efccf905ca2fc85386a428a32586562
StarCCC <810405089@qq.com>
2018-06-28 02:10:23 +0800

    6.28 新增图片

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,5 +1,12 @@
+import gui.MainFrame;
+
 /**
  * Created by liuyupeng@hey900.com on 2018/4/11.
  */
 public class AppMain {
+
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
 }
---
+++ src/main/java/gui/Board.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+
+    public Board(){
+        //设置棋盘大小
+        setSize(500, 400);
+        setBackground(Color.white);
+    }
+
+
+}
